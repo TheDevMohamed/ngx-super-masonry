4daf73642121b58210e4941a22349589c3e696e7
TheDevMohamed <alkhenaizi.mohamed@gmail.com>
2025-02-27 01:26:06 +0300

    Removed gem masonry component

--- projects/ngx-super-masonry/src/public-api.ts
+++ projects/ngx-super-masonry/src/public-api.ts
@@ -4,6 +4,4 @@
 
 export * from './lib/Components/super-masonry/ngx-super-masonry.component';
 export * from './lib/Components/masonry-item/masonry-item.component';
-export * from './lib/Components/gem-masonry/gem-masonry.component';
-
 
